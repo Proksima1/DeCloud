6af42d13dd16ffddb85af9ff25f9d688e6e0e419
Kirka <kirill.kobzev5555@gmail.com>
2024-12-03 23:45:36 +0300

    start

---
+++ RestApi/Prediction/views.py
@@ -0,0 +1,31 @@
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+   
+@api_view(['GET', 'POST'])
+def api_add(request):
+    sum = 0
+    response_dict = {}
+    if request.method == 'GET':
+        # Do nothing
+        pass
+    elif request.method == 'POST':
+        # Add the numbers
+        data = request.data
+        for key in data:
+            sum += data[key]
+        response_dict = {"sum": sum}
+    return Response(response_dict, status=status.HTTP_201_CREATED)
+
+
+class Add_Values(APIView):
+    def get(self, request, format=None):
+        sum = 0
+        # Add the numbers
+        data = request.data
+        for key in data:
+            sum += data[key]
+        response_dict = {"sum": sum}
+        return Response(response_dict, status=status.HTTP_201_CREATED)
