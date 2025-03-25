f6f7dadd714ef3e8adf7077a10ed0af2df397246
Kirka <kirill.kobzev5555@gmail.com>
2025-03-25 11:06:31 +0300

    CLOUD-48: fix: add expires_in

--- functions/generate-presigned-url/index.py
+++ functions/generate-presigned-url/index.py
@@ -7,8 +7,8 @@ AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
 
 def handler(event, context):
-    print("Raw event:", event)  # Логируем входные данные
-    # Если event — словарь, но тело запроса (`body`) передано строкой, парсим `body`
+    print("Raw event:", event)
+
     if isinstance(event, dict) and "body" in event:
         try:
             event = json.loads(event["body"])
@@ -17,9 +17,20 @@ def handler(event, context):
                 "statusCode": 400,
                 "body": json.dumps({"error": "Invalid JSON format in body", "event": event})
             }
-    print("Parsed event:", event)  # Логируем распарсенные данные
+
+    print("Parsed event:", event)
+
     bucket_name = event.get("bucket_name")
     task_id = event.get("task_id")
+    expires_in = event.get("expires_in", 3600)  # По умолчанию 3600 секунд (1 час)
+
+    # Проверка
+    if not bucket_name or not task_id:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Missing required parameters: 'bucket_name' and 'task_id' are required"})
+        }
+
     try:
         s3 = boto3.client(
             "s3",
@@ -33,23 +44,27 @@ def handler(event, context):
             "statusCode": 500,
             "body": json.dumps({"error": f"Ошибка при создании S3 клиента: {str(e)}"})
         }
-    file_key = f"testka/uploads/{task_id}" # # Формируем путь к файлу в S3
+
+    file_key = f"testka/uploads/{task_id}"
+
     try:
         presigned_url = s3.generate_presigned_url(
-            "put_object", # Генерация Pre-Signed URL
+            "put_object",
             Params={
                 "Bucket": bucket_name,
                 "Key": file_key,
                 "ContentType": "application/octet-stream",
             },
-            ExpiresIn=3600,
+            ExpiresIn=expires_in,
             HttpMethod="PUT"
         )
+
         return {
             "statusCode": 200,
             "body": json.dumps({
                 "presigned_url": presigned_url,
-                "task_id": task_id
+                "task_id": task_id,
+                "expires_in": expires_in
             })
         }
     except Exception as e:
